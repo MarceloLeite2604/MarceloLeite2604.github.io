37f9e93b32e8ebffce0c2b4004a2ee3789b04420
MarceloLeite2604 <marceloleite2604@gmail.com>
2020-08-06 07:52:00 -0300

    Adjust backend server address

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2bdf92adf4982d0951ffc974483a4b1d.js"
+  "/precache-manifest.2ae26f02f909cd487aece5f9bb0adc03.js"
 );
 
 self.addEventListener('message', (event) => {
